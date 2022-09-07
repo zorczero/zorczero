686a6abf0c6fba5f3fd6daba0ab54ee6e2d630ab
NautolanGT <113027887+NautolanGT@users.noreply.github.com>
2022-09-07 21:18:40 +0700

    Update Header.lua

--- Header.lua
+++ Header.lua
@@ -1,20 +1,22 @@
---=======SETUP=======--
+bot_raw = {}
+---======================---
+bot_raw["NAMEBOT1"] = {
 webhooks = {
-  ["LINK"]  = 'https://discord.com/api/webhooks/1016344127094403122/2fpU640ycUyRbVpNE8BRHmPwnyYhhWe4S0UQR_1h6RU8I1K9R1DQA9rZiv4THEO0uveb',
+  ["LINK"]  = '',
   ["EDIT"]  = false
-}
+},
 -----------------------
 bot_config = {
-  ["World"] = {"UIWHM"},
-  ["Door"] = "NAUASU12",
-  ["Loop"] = false,
+  ["World"] = {"FARM1"},
+  ["Door"] = 'FARM_ID',
+  ["Loop"] = true,
   ["Save_Mode"] = 1,
   ["Save"] = {
               {
-                ["World"] = "FOODLAUT",
-                ["Door"] = "PACK",       --Mode 1
-                ["Path_Seed"] = 6390,
-                ["Path_Pack"] = 954
+                ["World"] = "World_Save",
+                ["Door"] = "Door_ID",       --Mode 1
+                ["Path_Seed"] = 20,
+                ["Path_Pack"] = 12
               },
               {
                 ["World_Pack"] = "World_Save_Pack",
@@ -26,14 +28,14 @@ bot_config = {
                 ["Path_Seed"] = 20
               }
              }
-}
+},
 ------------------------
 bot_mode = {
-  ["Block"] = 4584,
+  ["Block"] = 5666,
   ["Type"] = 'fg',
   ["Ignore Gems"] = false, --Ignore Gems When Harvesting
-  ["Coordinate_Break"] = {3,1}
-}
+  ["Coordinate_Break"] = {2,1}
+},
 ------------------------
 bot_environment = {
   ["Trash"] = {
@@ -54,7 +56,7 @@ bot_environment = {
             },
   ["Jammer"] = true,
   ["Ban"] = {
-            ["Mode"] = false,
+            ["Mode"] = true,
             ["Ignore"] = {"Ignored_Name"}
             },
   ["Leave"] = {
@@ -62,4 +64,72 @@ bot_environment = {
             ["Limit"] = 5,
             ["Delay"] = 30 --Second
               }
+}
+}
+
+bot_raw["NAMEBOT2"] = {
+  webhooks = {
+  ["LINK"]  = '',
+  ["EDIT"]  = false
+},
+-----------------------
+bot_config = {
+  ["World"] = {"FARM1"},
+  ["Door"] = 'FARM_ID',
+  ["Loop"] = true,
+  ["Save_Mode"] = 1,
+  ["Save"] = {
+              {
+                ["World"] = "World_Save",
+                ["Door"] = "Door_ID",       --Mode 1
+                ["Path_Seed"] = 20,
+                ["Path_Pack"] = 12
+              },
+              {
+                ["World_Pack"] = "World_Save_Pack",
+                ["Door_Pack"] = "Door_ID_Save_Pack",
+                ["Path_Pack"] = 12,
+                                                      --Mode 2
+                ["World_Seed"] = "World_Save_Seed",     
+                ["Door_Seed"] = "Door_ID_Save_Seed",
+                ["Path_Seed"] = 20
+              }
+             }
+},
+------------------------
+bot_mode = {
+  ["Block"] = 5666,
+  ["Type"] = 'fg',
+  ["Ignore Gems"] = false, --Ignore Gems When Harvesting
+  ["Coordinate_Break"] = {2,1}
+},
+------------------------
+bot_environment = {
+  ["Trash"] = {
+              ["Limit"] = 120,
+              ["List"] = {5040,5042,5044,5032,5034,5036,5038,5024,5026,5028,5030,7164,7162}
+              },
+  ["Buy"] = {
+            ["Mode"] = true,
+            ["Name"] = 'world_lock',
+            ["Price"] = 2000,
+            ["Item_List"] = {242},
+            ["Limit"] = 3,
+            ["When"] = {
+                        ["Harvest"] = true,
+                        ["Plant"]   = true,
+                        ["Break"]   = true
+                       }
+            },
+  ["Jammer"] = true,
+  ["Ban"] = {
+            ["Mode"] = true,
+            ["Ignore"] = {"Ignored_Name"}
+            },
+  ["Leave"] = {
+            ["Mode"] = true,
+            ["Limit"] = 5,
+            ["Delay"] = 30 --Second
+              }
+}
 }

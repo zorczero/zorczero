58ce455bf3c7870da7dcf98f19f2d502f7a08bbe
zorczero <113151741+zorczero@users.noreply.github.com>
2022-09-10 12:49:31 +0700

    Update Header.lua

--- Header.lua
+++ Header.lua
@@ -1,6 +1,6 @@
 bot_raw = {}
 ---======================---
-bot_raw["rotasirival1"] = {
+bot_raw["rotasirival2"] = {
 webhooks = {
   ["LINK"]  = 'https://discord.com/api/webhooks/1016891366183288893/pwf7bGfZavkb7M2yO338dcOuH2JTmoDlwbiiKeOrLnrgPHvE9xRHc6Ia1FO62ICtuYgG',
   ["EDIT"]  = false

e7893a7f82d266509943785fa5d8603d0d110e88
zorczero <113151741+zorczero@users.noreply.github.com>
2022-09-11 14:13:15 +0700

    Update Header.lua

--- Header.lua
+++ Header.lua
@@ -75,7 +75,7 @@ bot_raw["rotasirival3"] = {
 -----------------------
 bot_config = {
   ["World"] = {"ITNVE","XRNRF","ASSVK","KMBFARM3","O2LIM","MRYMC66","BCMBO","FAKAFA7","SATAKILO","FGJVN","JAWQP","NDUWY","UHCGV","WAWSV","QSWXV","RELWT","XMCNO","VGYAZ","PEDTOQ","XBYGA","WEYQ2","ANOQK","DAHBB","MBVRN"},
-  ["Door"] = 'QBFGXZVRWMJ',
+  ["Door"] = 'QBFGXZVRW3X',
   ["Loop"] = true,
   ["Save_Mode"] = 1,
   ["Save"] = {
